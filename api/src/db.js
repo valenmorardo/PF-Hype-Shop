1c5412296c2fff42f6418edaa6727577b95d72bf
Bryan Millon <bryanmillon31@gmail.com>
2022-11-09 17:13:57 -0600

    Creacion ruta get y cambio en la db

--- api/src/db.js
+++ api/src/db.js
@@ -5,8 +5,8 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+//
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/sneakers`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
