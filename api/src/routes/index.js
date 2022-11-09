af449bf40921f6ff8e558016ffef9fb0d9ab5027
Bryan Millon <bryanmillon31@gmail.com>
2022-11-09 17:31:40 -0600

    Modularizacion back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,47 +1,12 @@
-const SneaksAPI = require('sneaks-api');
-const sneaks = new SneaksAPI();
+// const SneaksAPI = require('sneaks-api');
+// const sneaks = new SneaksAPI();
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
+const getSneakers = require ("./components/getSneakers")
 
 const router = Router();
 
 
-
-// sneaks.getProductPrices("FY7006", function(err, product){
-//     console.log(product)
-// })
-// const alt = sneaks.getProducts("sneakers", 10, function(err, products){
-//     let sneakers = []
-//     sneakers.push(products)
-//     let sneakersJson = {...sneakers}
-//     let filterInfo = sneakersJson["0"]?.map(e=>{
-//                 name: e.shoeName
-//         })
-//         return filterInfo
-// })
-
-router.get('/sneakers', async(req,res)=>{
-    sneaks.getProducts("sneakers", 51, function(err, products){
-
-        const productoFiltrado= products.map(el=>{
-            return{
-                id: el._id,
-                name:el.shoeName,
-                price: el.retailPrice,
-                brand: el.brand,
-                styleId: el.styleID,
-                colors: el.colorway,
-                img: el.thumbnail,
-            }})
-        try {
-            res.status(200).send(productoFiltrado)
-        } catch (error) {
-            console.log(error);
-        }
-    })
-})
+router.use("/sneakers", getSneakers)
 
 
 module.exports = router;
