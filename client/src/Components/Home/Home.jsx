f5c6f49af53c7cf36e4fc8be3a331de2195b17a9
Valentin <valen.morardo18@gmail.com>
2022-11-10 17:44:33 -0300

    pequeños cambios en home, creacion estrcutura componentes, creacion de un par de rutas

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,42 +1,40 @@
-import React from 'react'
+import React from "react";
 
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { getSneakers } from '../../Redux/actions/index';
+import { useDispatch, useSelector } from "react-redux";
+import { getSneakers } from "../../Redux/actions/index";
 
 import Card from "../Card/Card/Card";
 
 const Home = () => {
-  const dispatch = useDispatch()
-  const sneakers = useSelector((state) => state.allSneakers)
+  const dispatch = useDispatch();
+  const sneakers = useSelector((state) => state.allSneakers);
 
   useEffect(() => {
     dispatch(getSneakers());
-  }, [dispatch])
+  }, [dispatch]);
 
-  if (sneakers.length === 0) {
-    return (
-      <div>
-      <h1>cargando...</h1>
-      </div>
-    )
-  } else {
+  {
 
-    sneakers.map(e => {
-
-      return (
-        <div>
-          <h5>Hype Shop</h5>
-          <div key={e.id}>
-            <Card
-              id={e.id}
-              name={e.name}
-              img={e.img}
-            />
+        sneakers.length === 0? (
+          <div>
+            <h1>cargando...</h1>
           </div>
-        </div>
-      )
-    })
+          ) 
+        :
+          (
+            sneakers.map((e) => {
+              return (
+                <div>
+                  <h5>Hype Shop</h5>
+                  <div key={e.id}>
+                    <Card id={e.id} name={e.name} img={e.img} price={e.price}/>
+                  </div>
+                </div>
+              );
+            })
+          );
+
   }
-}
-export default Home
+};
+export default Home;
--- client/src/Redux/reducer/index.js
+++ client/src/Redux/reducer/index.js
@@ -1,20 +1,20 @@
-import{
-    GET_SNEAKERS
-}from "../actions/actionTypes"
-const initialState={
-    allSneakers:[],
-}
+import { GET_SNEAKERS } from "../actions/actionTypes";
 
-const rootReducer=(state=initialState,action)=>{
-    switch (action.type) {
-        case GET_SNEAKERS:
-            return{
-                ...state,
-                allSneakers:action.payload
-            }
-            default:
-        return  initialState;
-    }
-}
+const initialState = {
+  allSneakers: [],
+  
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SNEAKERS:
+      return {
+        ...state,
+        allSneakers: action.payload,
+      };
+    default:
+      return initialState;
+  }
+};
 
 export default rootReducer;
