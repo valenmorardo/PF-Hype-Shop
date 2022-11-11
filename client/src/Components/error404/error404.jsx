80e3e2ae35b02ea17fb471802fe64f1a1477b21f
Bryan Millon <bryanmillon31@gmail.com>
2022-11-11 13:50:19 -0600

    Añado error 404

---
+++ client/src/Components/error404/error404.jsx
@@ -0,0 +1,11 @@
+// import styles from '../error404/error404.module.css'
+
+export default function Error404(){
+
+    return(
+        <div>
+            <h1 >Error 404</h1>
+            <h3 >Invalid URL</h3>
+        </div>
+    )
+}
