f4fe9caee9ebb4f5813151c4324be1aaa7c44d03
sebastian prado <sebaslkjh@gmail.com>
2022-11-11 19:09:11 -0500

    Estilos Temporales CSS Cards y Card

--- client/src/Components/Cards/Card.jsx
+++ client/src/Components/Cards/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ title, pictures, price }) => {
     <div>
       <div className={styles.container}>
         <h3>{title}</h3>
-        <img src={pictures} alt="not image" width="40%" height="40%"></img>
+        <img className={styles.image} src={pictures} alt="not image" width="40%" height="40%"></img>
         <h3>$ {price}</h3>
       </div>
     </div>
