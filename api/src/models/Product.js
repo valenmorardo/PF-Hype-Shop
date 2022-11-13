70cebe800e96787cd13410e2d075248fa5eebbb5
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-13 19:51:52 -0300

    Modificado string a numero

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -20,7 +20,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       price: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
       condition: {
