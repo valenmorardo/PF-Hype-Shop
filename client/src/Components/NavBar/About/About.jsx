fe61e44810e3032e1c0ec52a5130b6b92d094826
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-12 23:06:10 -0300

    asd

--- client/src/Components/NavBar/About/About.jsx
+++ client/src/Components/NavBar/About/About.jsx
@@ -1,214 +1,108 @@
-import React from 'react'
-import styles from '../About/About.module.css'
+import React from "react";
+import styles from "../About/About.module.css";
 
 const About = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.aboutContainer}>
-        <h1 className={styles.title}>About us</h1>
-          <div className={styles.text}>
-              <p className={styles.text}>
-                We are a group of dedicated programmers who enjoy working as a team. During this experience we learned a lot about programming and about communication and working together.
-              </p>
-          </div>
+    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+      <div className="text-center pb-12">
+        <h2 className="text-base font-bold text-indigo-600">
+          We have the best equipment
+        </h2>
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+          Check our awesome team members
+        </h1>
       </div>
-        
-        <h1 className={styles.title}>Our Team</h1>
-          <div className={styles.teamsContainer}>
-            <div className={styles.box}>
-              <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/D4D35AQG0Ff8fe5EFHw/profile-framedphoto-shrink_200_200/0/1664543314586?e=1668877200&v=beta&t=ZM_1fRC9kR_AsGB-p0H53PTDg15NgRyNw6quTRr0ZNc'/>
-              </div>
-              <div className={styles.infoBox}>
-                <h3>Luciano Paniccia</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/luciano-paniccia-847868232/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/Luciano-Paniccia-Git' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/D4E35AQEmu8vMzXD0vg/profile-framedphoto-shrink_200_200/0/1666981054470?e=1668877200&v=beta&t=9Z2J2hm82xO9Zw5xndgYZGN02D-z1AJ7On_Rm6wlOYA'/>
-                <h3>Sebastian Prado Escobar</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/sebastian-prado-escobar-dev/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/sebas-pr2000' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/D4D35AQFhSgnc8xSdtA/profile-framedphoto-shrink_200_200/0/1667352021537?e=1668877200&v=beta&t=ubUTMNsDBVKXCVh6_OyteM0mvRoW26bgxorq6XBPBWU'/>
-                <h3>Valentin Morardo</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/valentin-morardo-b125ba240/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/Littyfever' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/D4E03AQFSZBhvUk8J6w/profile-displayphoto-shrink_200_200/0/1661831642590?e=1673481600&v=beta&t=voOglWcGfrt_tux1pSES8JORxF8_bbxMFznUbAhvTWg'/>
-                <h3>Bryan Francisco  Millon Ojeda</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/bryan-mill%C3%B3n/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/BryanMillon' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/C4E03AQFt1ZKcx8k7Jw/profile-displayphoto-shrink_200_200/0/1624726135332?e=1673481600&v=beta&t=FccVpKGSN0qSWQtZ0LVPhgRe_f5-TMPz0xi0cW1pcUM'/>
-                <h3>Mauro Sergio Damian Ferrera</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/damian-f/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/damianf2022' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/C4E03AQErx0bBN0gzVA/profile-displayphoto-shrink_200_200/0/1567744420647?e=1673481600&v=beta&t=1Ekpe1yRlnYQ17lMfKBe1TWUbxpqWGzqPnQa7VB9IhQ'/>
-                <h3>Santiago Gabriel Acuña</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/santiago-gabriel-acu%C3%B1a-40a116190/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/Deplover09' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            <div className={styles.box}>
-            <div>
-                <img className={styles.imgBox} alt="team" src='https://media-exp1.licdn.com/dms/image/C4D03AQFM0o5cWQ8W4w/profile-displayphoto-shrink_200_200/0/1647369973219?e=1673481600&v=beta&t=SuTfLynBvhe96-eUilG2SjHlGZ98kXrHsUPW_5hNoSY'/>
-                <h3>Braian Denis Gomez</h3>
-                <h4>Full-Stack Developer</h4>
-                <span className='links-container'>
-                  <a href='https://www.linkedin.com/in/braigomez/' target="_blank">
-                    <img
-                    src="https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/stikersDeContacto/linked-in-alt.svg" 
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                  <a href='https://github.com/braaidg' target='_blank'>
-                    <img
-                    src='https://raw.githubusercontent.com/AlanMauricioCastillo/Portfolio/master/public/GitHubImg.png'
-                    alt=''
-                    width='20px'
-                    height='20px'
-                    className={styles.icon}
-                    />
-                  </a>
-                </span>
-              </div>
-            </div>
-            </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+              alt="photo"
+            />
           </div>
-  )
-}
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
+            <p className="text-base text-gray-400 font-normal">
+              Software Engineer
+            </p>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+              alt="photo"
+            />
+          </div>
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Lucy Carter</p>
+            <p className="text-base text-gray-400 font-normal">
+              Graphic Designer
+            </p>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80"
+              alt="photo"
+            />
+          </div>
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Jade Bradley</p>
+            <p className="text-base text-gray-400 font-normal">Dev Ops</p>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+              alt="photo"
+            />
+          </div>
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
+            <p className="text-base text-gray-400 font-normal">
+              Software Engineer
+            </p>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+              alt="photo"
+            />
+          </div>
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Lucy Carter</p>
+            <p className="text-base text-gray-400 font-normal">
+              Graphic Designer
+            </p>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+          <div className="mb-8">
+            <img
+              className="object-center object-cover rounded-full h-36 w-36"
+              src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80"
+              alt="photo"
+            />
+          </div>
+          <div className="text-center">
+            <p className="text-xl text-gray-700 font-bold mb-2">Jade Bradley</p>
+            <p className="text-base text-gray-400 font-normal">Dev Ops</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
+
