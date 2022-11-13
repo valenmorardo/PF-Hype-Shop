e0413eb2e912a6406429131e8f48c05ff932dd0e
Valentin <valen.morardo18@gmail.com>
2022-11-13 16:08:23 -0300

    detalle de cada producto con toda la info necesaria, falta aplicar estilos

--- client/src/Components/CardDetail/CardDetail.jsx
+++ client/src/Components/CardDetail/CardDetail.jsx
@@ -1,31 +1,68 @@
-import React, {useEffect} from 'react'
-import { getDetail } from '../../Redux/actions/index';
+import React, { useEffect } from "react";
+import { getDetail } from "../../Redux/actions/index";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CardDetail = (props) => {
+  const dispatch = useDispatch();
+  const sneakerDetail = useSelector((state) => state.detail);
 
-  const dispatch = useDispatch()
-  const sneakerDetail = useSelector(state => state.detail)
+  useEffect(() => {
+    dispatch(getDetail(props.match.params.id));
+  }, [dispatch, props.match.params.id]);
 
-  useEffect(()=>{
-    dispatch(getDetail(props.match.params.id))
-  }, [dispatch, props.match.params.id])
+  const id = props.match.params.id;
 
-  console.log(sneakerDetail)
+  console.log(sneakerDetail);
   // console.log(Object.keys(sneakerDetail))
 
-  return (
-    <div>
-    <div>CardDetail</div>
+  if (id.length !== 36) {
 
-    {sneakerDetail?(
+    return (
       <div>
-        <h1>{sneakerDetail.title}</h1>
+        <h1>No existe producto con ese ID</h1>
+        <Link to="/home">
+          <button>VOLVER</button>
+        </Link>
       </div>
-    ):<p>Loading...</p>}
+    );
 
-    </div>
-  )
-}
+  } else
+    return (
+
+      <div>
+        {sneakerDetail ? (
+          <div>
+            <Link to="/home">
+              <button>VOLVER</button>
+            </Link>
+
+            <h1>{sneakerDetail.title}</h1>
+            <div>
+              {sneakerDetail.pictures?.map((e) => (
+                <img src={e} />
+              ))}
+            </div>
+
+            <div>
+              <div>
+                <span>TIPO DE CALZADO: {sneakerDetail.shoeStyle}</span>
+                <br />
+                <span>MARCA: {sneakerDetail.brand}</span>
+                <br />
+                <span>TALLA: {sneakerDetail.size}</span>
+                <br />
+                <span>PRECIO: ${sneakerDetail.price} ARS</span>
+                <br />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      
+    );
+};
 
 export default CardDetail;
