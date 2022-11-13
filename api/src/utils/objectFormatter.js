1b22fcb41d8c391be74f4fa410f89baf0c0bfa87
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-13 09:08:45 -0300

    Cambios en bd

--- api/src/utils/objectFormatter.js
+++ api/src/utils/objectFormatter.js
@@ -1,5 +1,16 @@
 const objectFormatter = (details) => {
-  const {id, title, price, condition, thumbnail, pictures, attributes } = details;
+  const {
+    id,
+    title,
+    price,
+    condition,
+    thumbnail,
+    pictures,
+    attributes,
+    variations,
+    date_created,
+    last_updated,
+  } = details;
 
   const requiredAttributes = [
     "BRAND",
@@ -19,7 +30,34 @@ const objectFormatter = (details) => {
       }
     }
   });
+
   const attributesObject = Object.fromEntries(results);
+
+  const colors = new Set();
+  const sizes = new Set();
+
+  if (!attributesObject.COLOR) {
+    variations.forEach((everyVariation) => {
+      const combination = everyVariation.attribute_combinations;
+      const colorVariant = combination[0];
+      let sizeVariant = combination[1];
+      sizeVariant.id === "SIZE"
+        ? (sizeVariant = combination[1])
+        : (sizeVariant = combination[2]);
+
+      colors.add(colorVariant.value_name);
+      sizes.add(sizeVariant.value_name.slice(0, 2));
+    });
+  }
+  const size =
+    attributesObject.SIZE === undefined
+      ? Array.from(sizes)
+      : [attributesObject.SIZE.slice(0, 2)];
+
+  const color =
+    attributesObject.COLOR === undefined
+      ? Array.from(colors)
+      : [attributesObject.COLOR];
 
   const objFormatted = {
     id,
@@ -30,10 +68,12 @@ const objectFormatter = (details) => {
     pictures: pictures.map((picture) => picture.url),
     age_group: attributesObject.AGE_GROUP || "",
     brand: attributesObject.BRAND || "",
-    color: attributesObject.COLOR || "",
+    colors: color,
     externalMaterial: attributesObject.EXTERIOR_MATERIALS || "",
     shoeStyle: attributesObject.FOOTWEAR_TYPE || "",
-    size: attributesObject.SIZE || "",
+    sizes: size,
+    date_created,
+    last_updated,
   };
 
   return objFormatted;
