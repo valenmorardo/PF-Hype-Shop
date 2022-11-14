c3f7db4307da3c84125e36f26b4d526e3e2b8212
braaidg <76708266+braaidg@users.noreply.github.com>
2022-11-14 18:17:06 -0300

    Update Product.js

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -7,8 +7,7 @@ module.exports = (sequelize) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
       unique: true,
-      allowNull: false,
-
+      allowNull: false
     },
     thumbnail: {
       type: DataTypes.STRING,
