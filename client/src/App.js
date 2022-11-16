75eeae2d46dbcdc9578fc59e4fa1280967451c3d
sebastian prado <sebaslkjh@gmail.com>
2022-11-16 16:46:52 -0500

    Creacion de Route para Carrito Ordenes

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,9 @@ import About from "./Components/NavBar/About/About";
 import CardDetail from "./Components/CardDetail/CardDetail";
 import CreateProduct from "./Components/CreateProduct/CreateProduct";
 import error404 from "./Components/error404/error404";
-<<<<<<< HEAD
-import SingIn from "./Components/NavBar/Registrar/SingIn";
+// import SingIn from "./Components/NavBar/Registrar/SingIn";
 // CARRITO
 import OrderCarry from "./Components/OrderCarry/OrderCarry";
-=======
->>>>>>> 0e33bab5890de5a513e669697c9689c118bba083
 
 function App() {
    return (
