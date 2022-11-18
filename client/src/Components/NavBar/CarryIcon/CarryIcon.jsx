500d1e762cf577c403a1e5a61cf6b2338adb5584
sebastian prado <sebaslkjh@gmail.com>
2022-11-18 18:25:31 -0500

    Cambiando icono de carrito con nuevo Contador

--- client/src/Components/NavBar/CarryIcon/CarryIcon.jsx
+++ client/src/Components/NavBar/CarryIcon/CarryIcon.jsx
@@ -8,13 +8,21 @@ const CarryIcon = () => {
 
     const [item, saveItem] = useLocalStorage("SNEAKERS", []);
 
-    const cantidad = item.length;
+    const totalItemsQuantity = (item) => {
+        const arrayPrecio = item.map(it => it.cantidad)
+        const sumaTotal = arrayPrecio.reduce((a, b) => a + b, 0)
+        return sumaTotal
+    }
+
+    let cantidad = totalItemsQuantity(item)
+
     return (
         <>
             <img className='w-7 h-auto object-cover' src={iconCart} />
+            {/* {console.log(totalItemsQuantity(item))} */}
             <span className='relative bottom-3 right-3' >
                 {cantidad !== 0 ?
-                    <BubbleAlert value={cantidad} />
+                    < BubbleAlert value={cantidad} />
                     : null
                 }
             </span>
