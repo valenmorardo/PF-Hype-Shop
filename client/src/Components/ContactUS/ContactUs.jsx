7b7ad78df3530f757ec50a20e91213ff8987122a
Bryan Millon <87747915+BryanMillon@users.noreply.github.com>
2022-11-21 13:46:04 -0600

    Arreglo la ruta del contact us

--- client/src/Components/ContactUS/ContactUs.jsx
+++ client/src/Components/ContactUS/ContactUs.jsx
@@ -91,7 +91,7 @@ export default function ContactUs() {
                                         type="hidden"
                                         name="_next"
                                         // ESto se debe cambiar al momento del DEPLOY
-                                        value="https://hype-shop.vercel.app/"
+                                        value="https://hype-gamma.vercel.app/"
                                     />
                                 </div>
                             </div>
