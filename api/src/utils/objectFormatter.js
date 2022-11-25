b6e342171def328a6b2dbd18395452b9b25e64c9
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-25 05:28:37 -0300

    Info reformateada a como estaba antes, para que anden los filtros

--- api/src/utils/objectFormatter.js
+++ api/src/utils/objectFormatter.js
@@ -1,5 +1,6 @@
 const objectFormatter = (details) => {
   const {
+    id,
     title,
     price,
     condition,
@@ -17,48 +18,29 @@ const objectFormatter = (details) => {
     "Género",
     "Estilo",
   ];
-  const results = [];
+  const attributesFormatted = {};
 
   attributes.forEach((attr) => {
     for (const attribute of requiredAttributes) {
-      if (attr.id === attribute) {
-        results.push({
-          name: attr.value_name,
-          value_name: attr.name,
-        });
+      if (attr.name === attribute) {
+        attributesFormatted[attribute] = attr.value;
       }
     }
   });
-
-  const arrayOfVariations = [];
-  if (variations.length) {
-    variations.forEach((variation) => {
-      arrayOfVariations.push({
-        price: variation.price,
-        sold_quantity: variation.sold_quantity,
-        available_quantity: variation.available_quantity,
-        picture_ids: variation.picture_ids,
-        attribute_combinations: variation.attribute_combinations,
-      });
-    });
-  }
 
   return {
     id,
     title,
     price: Number(price),
     condition,
-    thumbnail,
-    pictures: pictures.map((picture) => picture.url),
-    age_group: attributesObject.AGE_GROUP || "No especificado",
-    brand: attributesObject.BRAND || "No especificado",
-    colors: color,
-    externalMaterial: attributesObject.EXTERIOR_MATERIALS || "No especificado",
-    shoeStyle: attributesObject.FOOTWEAR_TYPE || "No especificado",
-    sizes: size,
-    last_updated,
-    gender: attributesObject.GENDER,
-    category: attributesObject.STYLE,
+    sold_quantity,
+    available_quantity,
+    variations,
+    pictures,
+    age_group: attributesFormatted.Edad,
+    brand: attributesFormatted.Marca,
+    gender: attributesFormatted["Género"],
+    category: attributesFormatted["Estilo"],
   };
 };
 
--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -3,190 +3,188 @@ import filter from "../../utils/filter";
 import orden from "../../utils/orden";
 
 import {
-   GET_SNEAKERS,
-   SEARCH_SNEAKER,
-   GET_DETAIL,
-   GET_BRANDS,
-   GET_CATEGORIES,
-   GET_GENDERS,
-   FILTER,
-   DETAIL_ZERO,
-   SET_CURRENT_USER,
-   // ORDER ADMIN
-   GET_ORDERS,
-   PATCH_ORDER,
-   FILTER_DATE,
-   FILTER_STATE,
+  GET_SNEAKERS,
+  SEARCH_SNEAKER,
+  GET_DETAIL,
+  GET_BRANDS,
+  GET_CATEGORIES,
+  GET_GENDERS,
+  FILTER,
+  DETAIL_ZERO,
+  SET_CURRENT_USER,
+  // ORDER ADMIN
+  GET_ORDERS,
+  PATCH_ORDER,
+  FILTER_DATE,
+  FILTER_STATE,
 } from "./actionTypes";
 
 export const getSneakers = () => {
-   return async (dispatch) => {
-      try {
-         const sneakers = await axios.get("http://localhost:3001/sneakers");
-         /* console.log(response) */
+  return async (dispatch) => {
+    try {
+      const sneakers = await axios.get("http://localhost:3001/sneakers");
+      /* console.log(response) */
 
-         return dispatch({
-            type: GET_SNEAKERS,
-            payload: sneakers.data,
-         });
-      } catch (error) {
-         console.log("error trying to GET_SNEAKERS", error);
-      }
-   };
+      return dispatch({
+        type: GET_SNEAKERS,
+        payload: sneakers.data,
+      });
+    } catch (error) {
+      console.log("error trying to GET_SNEAKERS", error);
+    }
+  };
 };
 
 export const getSearchTitle = (title) => {
-   return async function (dispatch) {
-      try {
-         const response = await axios.get(
-            `http://localhost:3001/sneakers?title=${title}`
-         );
-         return dispatch({
-            type: SEARCH_SNEAKER,
-            payload: response.data,
-         });
-      } catch (error) {
-         console.log("error trying to SEARCH_SNEAKER");
-      }
-   };
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/sneakers?title=${title}`
+      );
+      return dispatch({
+        type: SEARCH_SNEAKER,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log("error trying to SEARCH_SNEAKER");
+    }
+  };
 };
 
 /* CREAR PRODUCTO */
 
 export function CreateNewProduct(payload) {
-   return async function () {
-      const response = await axios.post(
-         "http://localhost:3001/sneakersCreate",
-         payload
-      );
-      return response;
-   };
+  return async function () {
+    const response = await axios.post(
+      "http://localhost:3001/sneakersCreate",
+      payload
+    );
+    return response;
+  };
 }
 
 export function getDetail(id) {
-   return async function (dispatch) {
-      try {
-         let json = await axios.get(`http://localhost:3001/sneakers/${id}`);
-         return dispatch({
-            type: GET_DETAIL,
-            payload: json.data,
-         });
-      } catch (error) {
-         return alert("Detail not found");
-      }
-   };
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`http://localhost:3001/sneakers/${id}`);
+      return dispatch({
+        type: GET_DETAIL,
+        payload: json.data,
+      });
+    } catch (error) {
+      return alert("Detail not found");
+    }
+  };
 }
 
 export function getBrands() {
-   return async function (dispatch) {
-      try {
-         let json = await axios.get(`http://localhost:3001/filters/brand`);
-         json.data.unshift("Todos");
-         let marcas = json.data.filter((e) => e != null);
-         return dispatch({
-            type: GET_BRANDS,
-            payload: marcas,
-         });
-      } catch (error) {
-         return alert("Brands not found");
-      }
-   };
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`http://localhost:3001/filters/brand`);
+      json.data.unshift("Todos");
+      let marcas = json.data.filter((e) => e != null);
+      return dispatch({
+        type: GET_BRANDS,
+        payload: marcas,
+      });
+    } catch (error) {
+      return alert("Brands not found");
+    }
+  };
 }
 
 export function getCategories() {
-   return async function (dispatch) {
-      try {
-         let json = await axios.get(
-            `https://hype-shop.vercel.app/filters/category`
-         );
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`http://localhost:3001/filters/category`);
 
-         json.data.unshift("Todos");
-         let marcas = json.data.filter((e) => e != null);
+      json.data.unshift("Todos");
+      let marcas = json.data.filter((e) => e != null);
 
-         return dispatch({
-            type: GET_CATEGORIES,
-            payload: marcas,
-         });
-      } catch (error) {
-         return alert("Brands not found");
-      }
-   };
+      return dispatch({
+        type: GET_CATEGORIES,
+        payload: marcas,
+      });
+    } catch (error) {
+      return alert("Brands not found");
+    }
+  };
 }
 
 export function getGenders() {
-   return async function (dispatch) {
-      try {
-         let json = await axios.get(`http://localhost:3001/filters/gender`);
-         json.data.unshift("Todos");
-         let generos = json.data.filter((e) => e != null && e != "Men");
+  return async function (dispatch) {
+    try {
+      let json = await axios.get(`http://localhost:3001/filters/gender`);
+      json.data.unshift("Todos");
 
-         return dispatch({
-            type: GET_GENDERS,
-            payload: generos,
-         });
-      } catch (error) {
-         return alert("Brands not found");
-      }
-   };
+      return dispatch({
+        type: GET_GENDERS,
+        payload: json.data,
+      });
+    } catch (error) {
+      return alert("Brands not found");
+    }
+  };
 }
 
 export const filterSneakers = (payload) => {
-   const functionFilter = (products) => {
-      const filteredProducts = filter(payload.filtros, products);
-      return orden(payload.orden, filteredProducts);
-   };
-   return {
-      type: FILTER,
-      payload: {
-         filter: functionFilter,
-         filtros: payload.filtros,
-         orden: payload.orden,
-      },
-   };
+  const functionFilter = (products) => {
+    const filteredProducts = filter(payload.filtros, products);
+    return orden(payload.orden, filteredProducts);
+  };
+  return {
+    type: FILTER,
+    payload: {
+      filter: functionFilter,
+      filtros: payload.filtros,
+      orden: payload.orden,
+    },
+  };
 };
 
 // DESMONTANDO COMPONENTE DETAIL
 export const setCurrentUser = (user) => {
-   return {
-      type: SET_CURRENT_USER,
-      payload: user,
-   };
+  return {
+    type: SET_CURRENT_USER,
+    payload: user,
+  };
 };
 
 export const detailZero = () => {
-   return {
-      type: DETAIL_ZERO,
-      payload: [],
-   };
+  return {
+    type: DETAIL_ZERO,
+    payload: [],
+  };
 };
 
 export const getOrders = () => {
-   return async (dispatch) => {
-      try {
-         const orders = await axios.get("http://localhost:3001/orders");
+  return async (dispatch) => {
+    try {
+      const orders = await axios.get("http://localhost:3001/orders");
 
-         return dispatch({
-            type: GET_ORDERS,
-            payload: orders.data,
-         });
-      } catch (error) {
-         console.log("error trying to GET_ORDERS", error);
-      }
-   };
+      return dispatch({
+        type: GET_ORDERS,
+        payload: orders.data,
+      });
+    } catch (error) {
+      console.log("error trying to GET_ORDERS", error);
+    }
+  };
 };
 
 // SE MANDA EL ID POR QUERY Y INFO POR BODY
 export const patchOrder = (id, payload) => {
-   return async () => {
-      try {
-         const order = await axios.patch(
-            `http://localhost:3001/orders/?id=${id}`,
-            payload
-         );
+  return async () => {
+    try {
+      const order = await axios.patch(
+        `http://localhost:3001/orders/?id=${id}`,
+        payload
+      );
 
-         return order;
-      } catch (error) {
-         console.log("error trying to GET_ORDERS", error);
-      }
-   };
+      return order;
+    } catch (error) {
+      console.log("error trying to GET_ORDERS", error);
+    }
+  };
 };
+
