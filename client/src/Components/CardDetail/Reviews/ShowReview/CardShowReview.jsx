1ed2f114aa1a188791e91462d82b283ff75ecc09
Valentin Morardo <102821159+Littyfever@users.noreply.github.com>
2022-11-25 16:34:38 -0300

    Update CardShowReview.jsx
    cambio de estilos al div

--- client/src/Components/CardDetail/Reviews/ShowReview/CardShowReview.jsx
+++ client/src/Components/CardDetail/Reviews/ShowReview/CardShowReview.jsx
@@ -6,7 +6,7 @@ const CardShowReview = ({review}) => {
 
   return (
     
-    <div class=" border-solid border-2 border-black pb-10" > 
+    <div className="bg-gray-50 border border-blue-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 mb-10  w-2/4"> 
  
         {
           review.userId === null?
