d15e3750ccfcc4fb04df75f0630ec26e13ed985c
sebastian prado <sebaslkjh@gmail.com>
2022-11-26 16:51:29 -0500

    Quitando Decimales Del Precio en las Card del Home

--- client/src/Components/Cards/Card.jsx
+++ client/src/Components/Cards/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ title, pictures, price , id}) => 
+const Card = ({ title, pictures, price, id }) =>
 //   return (
 //     <div>
 //       <div className={styles.container}>
@@ -13,19 +13,19 @@ const Card = ({ title, pictures, price , id}) =>
 //   );
 {
   return (
-    
-            <a key={id} href={id} className="group">
-              <div className="aspect-w-1 aspect-h-1  h-64 w-full overflow-hidden rounded-lg   xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={pictures[0]}
-                  alt={pictures[0]}
-                  className="max-h-200 w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-900">{title}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-800"> ${price}</p>
-            </a>
-        
+
+    <a key={id} href={id} className="group">
+      <div className="aspect-w-1 aspect-h-1  h-64 w-full overflow-hidden rounded-lg   xl:aspect-w-7 xl:aspect-h-8">
+        <img
+          src={pictures[0]}
+          alt={pictures[0]}
+          className="max-h-200 w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-900">{title}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-800"> ${Math.trunc(price)}</p>
+    </a>
+
   )
 }
 // 
