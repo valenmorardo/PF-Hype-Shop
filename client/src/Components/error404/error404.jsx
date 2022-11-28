40a9ebde5f6ff64fd5a5b19f34b800fef03ec803
Bryan Millon <87747915+BryanMillon@users.noreply.github.com>
2022-11-28 16:32:44 -0600

    Update error404.jsx

--- client/src/Components/error404/error404.jsx
+++ client/src/Components/error404/error404.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+export default function Error404(){
 
     return(
         <section className="flex items-center h-full p-16 ">
