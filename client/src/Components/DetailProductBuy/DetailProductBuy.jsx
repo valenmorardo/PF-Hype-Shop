65e25597c7266c7060c642d0d6b6b8651d100a73
sebastian prado <sebaslkjh@gmail.com>
2022-11-28 20:42:11 -0500

    Agregando CardDetailBuy en Componente AdminOrders y Estilos

--- client/src/Components/DetailProductBuy/DetailProductBuy.jsx
+++ client/src/Components/DetailProductBuy/DetailProductBuy.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import DetailProductBuyCard from "./DetailProductBuyCard";
 
-const DetailProductBuy = ({ onClose, products }) => {
+const DetailProductBuy = ({ onClose, products, precioTotal }) => {
     const array = [1, 2, 3, 4, 5, 6, 7, 7, 87, 535, 434, 434]
     return (
-        <div className="z-20 bg-white max-w-full bg-contain min-h-[120rem] absolute top-0 bottom-0 right-0 left-0 rounded-2xl">
+        <div className="z-20 bg-white max-w-full bg-contain min-h-[40rem] absolute top-0 bottom-0 right-0 left-0 rounded-2xl">
             <div className="">
                 <div className="absolute left-0 top-4">
                     <button
@@ -15,7 +15,11 @@ const DetailProductBuy = ({ onClose, products }) => {
                     </button>
                 </div>
                 <div className="flex flex-col items-center  ">
-                    <p className="mt-3 font-bold tracking-tight text-indigo-700 sm:text-4xl mb-6" >Detalle Compra</p>
+                    <p className="mt-3 font-bold tracking-tight text-indigo-700 sm:text-4xl mb-4" >Detalle Compra</p>
+                    <div className="sticky top-3 flex gap-1 items-center bg-blue-500 p-3 rounded-xl text-white font-medium text-lg ">
+                        <p>Total Compra: </p>
+                        <p className="text-2xl"> ${precioTotal}</p>
+                    </div>
                     <div className="flex justify-center flex-wrap w-11/12 h-5/6 gap-4 ">
                         {products.length > 0 && products.map((el) => (
                             <DetailProductBuyCard
