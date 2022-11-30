2c4bcdfc0bea7444c9d552cb5502ca79be3845c2
Valentin Morardo <102821159+Littyfever@users.noreply.github.com>
2022-11-30 15:11:09 -0300

    Update Register.jsx

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -42,7 +42,7 @@ export default function Register() {
 
     const register = async () => {
         return await axios
-            .post("https://hype-shop.vercel.app/register", {
+            .post("http://localhost:3001/register", {
                 name,
                 email,
                 password,
