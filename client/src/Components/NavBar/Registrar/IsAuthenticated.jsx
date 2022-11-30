d32795be8c3825c4d6a82bdc88035a741bda0e15
Bryan Millon <87747915+BryanMillon@users.noreply.github.com>
2022-11-29 23:40:46 -0600

    Update IsAuthenticated.jsx

--- client/src/Components/NavBar/Registrar/IsAuthenticated.jsx
+++ client/src/Components/NavBar/Registrar/IsAuthenticated.jsx
@@ -14,7 +14,7 @@ const IsAuthenticated = () => {
   const dispatch = useDispatch();
 
   if (user) {
-    axios.post("http://localhost:3001/authentication", user).then((res) => {
+    axios.post("https://hype-shop.vercel.app/authentication", user).then((res) => {
       dispatch(setCurrentUser(res.data[0]));
     });
   }
