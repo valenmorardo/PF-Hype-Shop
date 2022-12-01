34f6c7c156a5f167e24fd25e2ed716e9494e7d6e
Santiago-Acuna <santfloo7xcrl8@gmail.com>
2022-12-01 11:42:51 -0300

    visible

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 
 const Cards = ({ sneakers }) => {
 
-
+const filterSneakers= sneakers.filter(d => d.visible === 0)
 
   return (
     // <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
@@ -19,7 +19,7 @@ const Cards = ({ sneakers }) => {
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
             <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {sneakers?.map((e) => (
+              {filterSneakers.map((e) => (
                 <Link to={`/sneaker/${e.id}`}>
                   <Card {...e} />
                   </Link>
