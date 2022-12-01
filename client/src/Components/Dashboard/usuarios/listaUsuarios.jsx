4aefc32f18f18e9a3bfe67bb815b4e7f3a69d7e1
Bryan Millon <bryanmillon31@gmail.com>
2022-12-01 02:20:41 -0600

    arreglo un poco los estilos de user

--- client/src/Components/Dashboard/usuarios/listaUsuarios.jsx
+++ client/src/Components/Dashboard/usuarios/listaUsuarios.jsx
@@ -29,7 +29,7 @@ const UsuariosCardsAdmin = ({ users}) => {
           
         </ul>
 
-            <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <div >
               {users?.map((e) => (
                 
                   <UsuariosCardAdmin {...e} />     
