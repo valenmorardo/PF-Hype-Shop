06a39c29674e3d4e3d4bb02d054016a429acc413
braaidg <76708266+braaidg@users.noreply.github.com>
2022-12-01 05:18:55 -0300

    Revert "Chequeado el descuento de cantidad"

--- client/src/Components/CardDetail/CardDetail.jsx
+++ client/src/Components/CardDetail/CardDetail.jsx
@@ -6,7 +6,7 @@ import "react-multi-carousel/lib/styles.css";
 import { Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
 import { useHistory } from "react-router-dom";
-import ShowReview from "./ShowReviews/ShowReview";
+import ShowReview from './ShowReviews/ShowReview'
 // CARRITO:
 import useLocalStorage from "../useLocalStorage/useLocalstorage";
 import swal from "sweetalert";
@@ -71,51 +71,44 @@ const CardDetail = (props) => {
     saveItem(arrayNewQuantity);
   };
 
+  
   const onAddCarry = (sneaker) => {
-    if (sneaker.available_quantity > 0) {
-      let carryItem = item.find((elem) => elem.id === sneaker.id);
-      // console.log("item", newItem)
-      if (carryItem) {
-        if (carryItem.cantidad <= sneaker.available_quantity - 1) {
-          const itemIndex = item.findIndex((it) => it.id === sneaker.id);
-          const newItem = [...item];
-          newItem.splice(itemIndex, 1);
-          newItem.push({
-            ...carryItem,
-            cantidad: carryItem.cantidad + 1,
-          });
-          saveItem(newItem);
-          return swal(
-            "¡Agregaste de nuevo este Producto!",
-            `Tu nueva Cantidad es: ${carryItem.cantidad + 1}`,
-            "success"
-          );
-        }
-        if (carryItem.cantidad === sneaker.available_quantity) {
-          return swal(
-            "No Stock",
-            "Tienes Todo el Stock que está disponible en tu Carrito.",
-            "error"
-          );
-        }
-      } else {
+    let carryItem = item.find((elem) => elem.id === sneaker.id);
+    // console.log("item", newItem)
+    if (carryItem) {
+      if (carryItem.cantidad <= sneaker.available_quantity - 1) {
+        const itemIndex = item.findIndex((it) => it.id === sneaker.id);
         const newItem = [...item];
+        newItem.splice(itemIndex, 1);
         newItem.push({
-          ...sneaker,
-          cantidad: 1,
+          ...carryItem,
+          cantidad: carryItem.cantidad + 1,
         });
         saveItem(newItem);
         return swal(
-          "Producto Agregado!",
-          "Este producto ahora Hace parte de tu Carrito!",
+          "¡Agregaste de nuevo este Producto!",
+          `Tu nueva Cantidad es: ${carryItem.cantidad + 1}`,
           "success"
         );
       }
+      if (carryItem.cantidad === sneaker.available_quantity) {
+        return swal(
+          "No Stock",
+          "Tienes Todo el Stock que está disponible en tu Carrito.",
+          "error"
+        );
+      }
     } else {
+      const newItem = [...item];
+      newItem.push({
+        ...sneaker,
+        cantidad: 1,
+      });
+      saveItem(newItem);
       return swal(
-        "No Stock",
-        "Tienes Todo el Stock que está disponible en tu Carrito.",
-        "error"
+        "Producto Agregado!",
+        "Este producto ahora Hace parte de tu Carrito!",
+        "success"
       );
     }
 
@@ -125,55 +118,47 @@ const CardDetail = (props) => {
   };
 
   const buy = (sneaker) => {
-    if (sneaker.available_quantity > 0) {
-      let carryItem = item.find((elem) => elem.id === sneaker.id);
-      // console.log("item", newItem)
-      if (carryItem) {
-        if (carryItem.cantidad <= sneaker.available_quantity - 1) {
-          const itemIndex = item.findIndex((it) => it.id === sneaker.id);
-          const newItem = [...item];
-          newItem.splice(itemIndex, 1);
-          newItem.push({
-            ...carryItem,
-            cantidad: carryItem.cantidad + 1,
-          });
-          saveItem(newItem);
-          history.push("/orderCarry");
-          return (
-            swal(
-              "¡Agregaste de nuevo este Producto!",
-              `Tu nueva Cantidad es: ${carryItem.cantidad + 1}`,
-              "success"
-            ),
-            history.push("/orderCarry")
-          );
-        }
-        if (carryItem.cantidad === sneaker.available_quantity) {
-          return swal(
-            "No Stock",
-            "Tienes Todo el Stock que está disponible en tu Carrito.",
-            "error"
-          );
-        }
-      } else {
+    let carryItem = item.find((elem) => elem.id === sneaker.id);
+    // console.log("item", newItem)
+    if (carryItem) {
+      if (carryItem.cantidad <= sneaker.available_quantity - 1) {
+        const itemIndex = item.findIndex((it) => it.id === sneaker.id);
         const newItem = [...item];
+        newItem.splice(itemIndex, 1);
         newItem.push({
-          ...sneaker,
-          cantidad: 1,
+          ...carryItem,
+          cantidad: carryItem.cantidad + 1,
         });
         saveItem(newItem);
         history.push("/orderCarry");
+        return (
+          swal(
+            "¡Agregaste de nuevo este Producto!",
+            `Tu nueva Cantidad es: ${carryItem.cantidad + 1}`,
+            "success"
+          ),
+          history.push("/orderCarry")
+        );
+      }
+      if (carryItem.cantidad === sneaker.available_quantity) {
         return swal(
-          "Producto Agregado!",
-          "Este producto ahora Hace parte de tu Carrito!",
-          "success"
+          "No Stock",
+          "Tienes Todo el Stock que está disponible en tu Carrito.",
+          "error"
         );
       }
     } else {
+      const newItem = [...item];
+      newItem.push({
+        ...sneaker,
+        cantidad: 1,
+      });
+      saveItem(newItem);
+      history.push("/orderCarry");
       return swal(
-        "No Stock",
-        "Tienes Todo el Stock que está disponible en tu Carrito.",
-        "error"
+        "Producto Agregado!",
+        "Este producto ahora Hace parte de tu Carrito!",
+        "success"
       );
     }
   };
@@ -182,8 +167,11 @@ const CardDetail = (props) => {
     setVariationChoosen(sneaker);
   };
 
+ 
   return (
+    
     <>
+   
       {variationChoosen.id ? (
         <div className="bg-white">
           <div className="pt-6">
@@ -198,7 +186,7 @@ const CardDetail = (props) => {
             {/* Product info */}
             <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
               <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                <h1 className="text-2xl font-bold tracking-tight  text-gray-900 sm:text-3xl">
                   {sneakerDetail.title}
                 </h1>
               </div>
@@ -218,69 +206,67 @@ const CardDetail = (props) => {
                     variations={sneakerDetail.variations}
                   />
                 ) : (
-                  <p className=" text-blue-700  text-sm  font-bold px-5 py-2.5 text-center mr-2 ">
-                    Stock disponible: {sneakerDetail.available_quantity}
-                  </p>
+                  <p className=" text-blue-700  text-sm  font-bold px-5 py-2.5 text-center mr-2 ">Stock disponible: {sneakerDetail.available_quantity}</p>
                 )}
 
-                {variationChoosen.variations.length === 0 ? (
+{variationChoosen.variations.length === 0?
+                <button
+                type="submit"
+                onClick={() =>
+                  onAddCarry({
+                    title: sneakerDetail.title,
+                    ...variationChoosen,
+                  })
+                }
+                
+                className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white border border-transparent rounded-md bg-lime-500 hover:bg-lime-400 focus:outline-none "
+                >
+                  Añadir Carrito
+                </button>
+                :      
                   <button
-                    type="submit"
-                    onClick={() =>
-                      onAddCarry({
-                        title: sneakerDetail.title,
-                        ...variationChoosen,
-                      })
-                    }
-                    className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white border border-transparent rounded-md bg-lime-500 hover:bg-lime-400 focus:outline-none "
+                  type="submit"
+                  onClick={() => swal(
+                    "No has elegido el color y el talle!",
+                    "Primero tienes que elegir el color y el talle",
+                    "error"
+                  )
+                  }
+                  className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white border border-transparent rounded-md bg-red-500 hover:bg-red-600 focus:outline-none "
                   >
                     Añadir Carrito
                   </button>
-                ) : (
+                  }
+              
+              {variationChoosen.variations.length === 0?
+                <button
+                type="submit"
+                onClick={() =>
+                  buy({
+                    title: sneakerDetail.title,
+                    ...variationChoosen,
+                  })
+                }
+                
+                className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 "
+                >
+                  Comprar
+                </button>
+                :      
                   <button
-                    type="submit"
-                    onClick={() =>
-                      swal(
-                        "No has elegido el color y el talle!",
-                        "Primero tienes que elegir el color y el talle",
-                        "error"
-                      )
-                    }
-                    className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white bg-red-500 border border-transparent rounded-md hover:bg-red-600 focus:outline-none "
+                  type="submit"
+                  onClick={() => swal(
+                    "No has elegido el color y el talle!",
+                    "Primero tienes que elegir el color y el talle",
+                    "error"
+                  )
+                  }
+                  className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white border border-transparent rounded-md bg-red-500 hover:bg-red-600 focus:outline-none "
                   >
-                    Añadir Carrito
+                     Comprar
                   </button>
-                )}
-
-                {variationChoosen.variations.length === 0 ? (
-                  <button
-                    type="submit"
-                    onClick={() =>
-                      buy({
-                        title: sneakerDetail.title,
-                        ...variationChoosen,
-                      })
-                    }
-                    className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 "
-                  >
-                    Comprar
-                  </button>
-                ) : (
-                  <button
-                    type="submit"
-                    onClick={() =>
-                      swal(
-                        "No has elegido el color y el talle!",
-                        "Primero tienes que elegir el color y el talle",
-                        "error"
-                      )
-                    }
-                    className="flex items-center justify-center w-full px-8 py-3 mt-10 text-base font-medium text-white bg-red-500 border border-transparent rounded-md hover:bg-red-600 focus:outline-none "
-                  >
-                    Comprar
-                  </button>
-                )}
-
+                  }
+              
                 {/* </form> */}
               </div>
 
